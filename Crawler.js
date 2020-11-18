ad47f3a4158e9bc1be7994cbb23ebfe765c792a3
BILLKATO <biel.mk@hotmail.com>
2020-11-18 00:05:22 -0300

    Teste

--- Crawler.js
+++ Crawler.js
@@ -36,7 +36,7 @@ function crawler(email) //Executa o Crawler
 		 '--no-sandbox',
 		 '--disable-setuid-sandbox'
 	 ],
-	 headless: true,
+	 headless: false,
 	 });
 
 	 const page = await browser.newPage(); 			//Abre o browser
@@ -49,7 +49,7 @@ function crawler(email) //Executa o Crawler
 		relatorio = relatorio.concat("-------------------------------------------------");
 		relatorio = relatorio.concat(email[i].email);
 	 	await page.goto(url); 							//Vai para a url definida
-	 	await page.waitForSelector('input#Account',{waitUntil: 'load', timeout: 0});
+	 	await page.waitForSelector('input#Account.form-control')//,{waitUntil: 'load', timeout: 0});
  	 	await page.$eval('input#Account',(el, value) => el.value = value, email[i].email);
 	 	await page.click('button#searchPwnage');
 	 	await page.waitFor(2000); 						//Espera 2 segundos para carregar a pagina do site
